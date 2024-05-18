c9fcb9d24eff76e4b84ebbf4d5a753273bb4b322
ghoshsourav99 <146316533+ghoshsourav99@users.noreply.github.com>
2024-05-18 12:53:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const endDate="04 november 2023 10:30:00 AM"
+document.getElementById("end-date").innerHTML=endDate;
+const inputs = document.querySelectorAll("input")
+// const clock=()=>{
+
+// }
+
+function clock(){
+  const end= new Date(endDate)
+  const now= new Date()
+  const diff= (end-now)/1000;
+  console.log(diff);
+  // console.log(end);
+  // console.log(now);
+
+  if(diff<0)return;
+inputs[0].value = Math.floor(diff/3600/24);
+inputs[1].value = (Math.floor(diff/3600)%24);
+inputs[2].value = (Math.floor(diff/60)%60);
+inputs[3].value = (Math.floor(diff%60));
+}
+clock()
+setInterval(
+  ()=>{
+    clock()
+  },
+  1000
+)
